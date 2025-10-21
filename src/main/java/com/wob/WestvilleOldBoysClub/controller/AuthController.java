4a28ede4fa36ad096c1b39955924a5ac03928ce2
ObakengPitse <55858479+ObakengPitse@users.noreply.github.com>
2025-10-21 19:08:03 +0200

    auth controller update

--- src/main/java/com/wob/WestvilleOldBoysClub/controller/AuthController.java
+++ src/main/java/com/wob/WestvilleOldBoysClub/controller/AuthController.java
@@ -9,15 +9,12 @@ import com.wob.WestvilleOldBoysClub.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/auth/ap1/v1")
+@RequestMapping("/auth/api/v1")
 public class AuthController {
     private final UserRepository userRepository;
     private final AdminRepository adminRepository;
